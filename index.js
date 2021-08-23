02bd52ec36de5ac23b29648e57df84680849f911
drestanto <dyas@live.com>
2021-08-23 16:23:02 +0700

    factorize code

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
+// ----------========== INITIATE ==========----------
+
 const express = require("express");
 const app = express();
 const port = 3000;
 
-function hello(req, res) {
-	res.send("Hello world!");
-}
-
 function start() {
-	console.log("Express Project listening at http://localhost:" + port);
+    console.log("Express project listening at http://localhost:" + port);
 }
-
-app.get('/', hello);
 
 app.listen(port, start);
+
+// ----------==============================----------
+
+// CONTROLLERS
+const indexController = require("./controller/index.js");
+
+// ROUTES
+app.get('/', indexController.index);
